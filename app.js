8afb1bb321e1c2adee27853b4eb3dda817b4f592
Adnan Balti <adnanqasmi267@gmail.com>
2023-02-02 11:11:06 +0500

    variablesForString

---
+++ app.js
@@ -0,0 +1,82 @@
+/*         Question-01: 
+Declare a variable called username.*/
+
+var username = "Muhammad Adnan";
+alert(username);
+
+
+/*         Question-02:
+Declare a variable called myName & assign to it a string 
+that represents your Full Name.*/
+
+var myName = "Muhammad Adnan";
+alert(myName);
+
+
+/*         Question-03: 
+Write script to
+a) Declare a JS variable, titled message.
+b) Assign “Hello World” to variable message
+c) Display the message in alert box.*/
+
+var titled = "Hello World!";
+alert(titled);
+
+
+/*         Question-04: 
+Write a script to save student’s bio data in JS variables and 
+show the data in alert boxes.
+VARIABLES FOR STRINGS | JAVASCRIPT
+Page 2 of 3*/
+
+var name = "Muhammad Adnan";
+alert("Hello I am" + " " + name);
+
+var age = 20;
+alert("I am" + " " + age + " " + "year old.");
+
+var profession = "Front end Web Developer";
+alert("I am" + " " + profession);
+
+
+/*         Question-05: 
+Write a script to display the following alert using one JS 
+variable:*/
+
+alert("PIZZA \n" + "PIZZ \n" + "PIZ \n" + "PI \n" + "P");
+
+
+/*         Question-06: 
+Declare a variable called email and assign to it a string that 
+represents your Email Address(e.g. example@example.com). 
+Show the blow mentioned message in an alert box.(Hint: use 
+string concatenation)*/
+
+var email = "adnanqasmi267@gmail.com";
+alert("My email Address is" + " " + email);
+
+
+/*         Question-07: 
+Declare a variable called book & give it the value “A
+smarter way to learn JavaScript”. Display the following
+message in an alert box:
+VARIABLES FOR STRINGS | JAVASCRIPT
+Page 3 of 3*/
+
+var book = "A smarter \n way to learn Javascript";
+alert("I am trying to learn from the book" + " " + book);
+
+
+/*         Question-08: 
+Write a script to display this in browser through JS*/
+
+var para = "Yah! I can write HTML content through JavaScript.";
+document.write(para);
+
+
+/*         Question-09: 
+Store following string in a variable and show in alert and 
+browser through JS */
+
+var endHash = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+alert(endHash);
